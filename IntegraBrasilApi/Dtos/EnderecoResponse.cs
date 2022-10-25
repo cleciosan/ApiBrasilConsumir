ae3d789df0708aad90cdfcdba423757850aaadc0
cleciosan <cleciosan@hotmail.com>
2022-10-25 17:08:59 -0300

    Criar na pasta Dtos a EnderecoResponse

---
+++ IntegraBrasilApi/Dtos/EnderecoResponse.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IntegraBrasilApi.Dtos
+{
+    public class EnderecoResponse
+    {
+        public string? Cep { get; set; }
+        public string? Estado { get; set; }
+        public string? Cidade { get; set; }
+        public string? Regiao { get; set; }
+        public string? Rua { get; set; }
+
+        [JsonIgnore]
+        public string? Servico { get; set; }
+    }
+}
