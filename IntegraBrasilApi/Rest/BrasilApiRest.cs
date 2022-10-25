34dbae80255c2b893ada9336cc890a8fb35ab8b1
cleciosan <cleciosan@hotmail.com>
2022-10-25 17:35:42 -0300

    Implementar Metodos da Interface IBrasilApi na BrasilApiRest

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -2,11 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Interfaces;
+using IntegraBrasilApi.Models;
 
 namespace IntegraBrasilApi.Rest
 {
-    public class BrasilApiRest
+    public class BrasilApiRest : IBrasilApi
     {
-        
+        public Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ResponseGenerico<BancoModel>> BuscarBanco(string codigoBanco)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
