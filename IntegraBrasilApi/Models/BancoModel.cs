35e5b0c0000dacce4dd1e09a8ca317a796fc4610
cleciosan <cleciosan@hotmail.com>
2022-10-25 11:55:55 -0300

    Fazer Tratamento de Nulo na Model BancoModel

--- IntegraBrasilApi/Models/BancoModel.cs
+++ IntegraBrasilApi/Models/BancoModel.cs
@@ -8,17 +8,17 @@ namespace IntegraBrasilApi.Models
 {
     public class BancoModel
     {
-        
-        public string Ispb { get; set; }
+        [JsonPropertyName("ispb")]
+        public string? Ispb { get; set; }
 
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [JsonPropertyName("code")]
-        public int Code { get; set; }
+        public int? Code { get; set; }
 
         [JsonPropertyName("fullName")]
-        public string FullName { get; set; }
+        public string? FullName { get; set; }
     }
 
 }
