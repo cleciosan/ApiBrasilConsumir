1c2ae3ea6361ff7e4b1356a0404edc5e33f7b449
cleciosan <cleciosan@hotmail.com>
2022-10-25 17:53:22 -0300

    Implementar programação para o Metodo BuscarEnderecoPorCep

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using IntegraBrasilApi.Dtos;
 using IntegraBrasilApi.Interfaces;
@@ -10,15 +11,31 @@ namespace IntegraBrasilApi.Rest
 {
     public class BrasilApiRest : IBrasilApi
     {
-        public Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
+        public async Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep(string cep)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/cep/v1/{cep}");
+
+            var response = new ResponseGenerico<EnderecoModel>();
+
+            using(var client = new HttpClient())
+            {
+                var responseBrasilApi = await client.SendAsync(request);
+                var contentResp = await responseBrasilApi.Content.ReadAsStringAsync();
+                var objResponse = JsonSerializer.Deserialize<EnderecoModel>(contentResp);
+            }
+
+            if (responseBrasilApi.IsSuccessStatusCode)
+            {
+                response.CodigoHttp = responseBrasilApi.StatusCode;
+                response.DadosRetorno = ObjResponse;
+            }   
         }
 
         public Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
         {
             throw new NotImplementedException();
         }
+
         public Task<ResponseGenerico<BancoModel>> BuscarBanco(string codigoBanco)
         {
             throw new NotImplementedException();
