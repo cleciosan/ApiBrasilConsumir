98581df32a3344e177227245650242672e0a4065
cleciosan <cleciosan@hotmail.com>
2022-10-25 10:58:27 -0300

    Criar as DataNotations ApiController e Route na EnderecoController

--- IntegraBrasilApi/Controllers/EnderecoController.cs
+++ IntegraBrasilApi/Controllers/EnderecoController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IntegraBrasilApi.Controllers
 {
+    [ApiController]
+    [Route("api/v1/[controller]")]
     public class EnderecoController : ControllerBase
     {
         
