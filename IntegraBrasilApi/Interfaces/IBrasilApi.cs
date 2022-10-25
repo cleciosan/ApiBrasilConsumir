108e01eda91a33ae350708761b48ea6af670d0e9
cleciosan <cleciosan@hotmail.com>
2022-10-25 17:24:07 -0300

    Criar Metodo BuscarEnderecoPorCep na Interface IBrasilApi

--- IntegraBrasilApi/Interfaces/IBrasilApi.cs
+++ IntegraBrasilApi/Interfaces/IBrasilApi.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Models;
 
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IBrasilApi
     {
-        
+        Task<ResponseGenerico<EnderecoModel>> BuscarEnderecoPorCep (string cep);
     }
 }
