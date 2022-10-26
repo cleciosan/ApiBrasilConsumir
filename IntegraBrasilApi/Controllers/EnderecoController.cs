33fbb5a6e39b01647a472858f826da3cfe4b7016
cleciosan <cleciosan@hotmail.com>
2022-10-26 13:08:37 -0300

    Configurar rota na EnderecoController

--- IntegraBrasilApi/Controllers/EnderecoController.cs
+++ IntegraBrasilApi/Controllers/EnderecoController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using IntegraBrasilApi.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,20 @@ namespace IntegraBrasilApi.Controllers
         public EnderecoController(IEnderecoService enderecoService)
         {
             _enderecoService = enderecoService;
+        }
+
+        public async Task<IActionResult> BuscarEndereco([FromRoute] string cep)
+        {
+            var response = await _enderecoService.BuscarEndereco(cep);
+
+            if(response.CodigoHttp == HttpStatusCode.OK)
+            {
+                return Ok(response.DadosRetorno);
+            }
+            else
+            {
+                return StatusCode((int)response.CodigoHttp, response.ErroRetorno);
+            }
         }
     }
 }
