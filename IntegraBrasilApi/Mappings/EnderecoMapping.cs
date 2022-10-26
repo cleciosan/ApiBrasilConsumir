2e3bff4cb7f5ebb93ec45d07be5c7a953eb39364
cleciosan <cleciosan@hotmail.com>
2022-10-26 12:10:02 -0300

    Implementar configuracao do AutoMapper na EnderecoMapping

---
+++ IntegraBrasilApi/Mappings/EnderecoMapping.cs
@@ -0,0 +1,17 @@
+
+using AutoMapper;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Models;
+
+namespace IntegraBrasilApi.Mappings
+{
+    public class EnderecoMapping : Profile
+    {
+        public EnderecoMapping()
+        {
+            CreateMap(typeof(ResponseGenerico<>),typeof(ResponseGenerico<>));
+            CreateMap<EnderecoResponse,EnderecoModel>();
+            CreateMap<EnderecoModel,EnderecoResponse>();
+        }
+    }
+}
