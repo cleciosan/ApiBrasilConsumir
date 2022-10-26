87449a0cd60f52a4764bfa118eedbaac5e15c6c9
cleciosan <cleciosan@hotmail.com>
2022-10-26 12:01:40 -0300

    Implementar Servico EnderecoServico

--- IntegraBrasilApi/Interfaces/IEnderecoService.cs
+++ IntegraBrasilApi/Interfaces/IEnderecoService.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IntegraBrasilApi.Dtos;
 
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IEnderecoService
     {
-        
+        Task<ResponseGenerico<EnderecoResponse>> BuscarEndereco (string cep);
     }
 }
--- IntegraBrasilApi/Services/EnderecoService.cs
+++ IntegraBrasilApi/Services/EnderecoService.cs
@@ -1,12 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Interfaces;
 
 namespace IntegraBrasilApi.Services
 {
-    public class EnderecoService
+    public class EnderecoService : IEnderecoService
     {
-        
+        private readonly IMapper _mapper;
+
+        private readonly IBrasilApi _brasilApi;
+
+        public async Task<ResponseGenerico<EnderecoResponse>> BuscarEndereco(string cep)
+        {
+            var endereco = await _brasilApi.BuscarEnderecoPorCep(cep);
+            return _mapper.Map<ResponseGenerico<EnderecoResponse>>(endereco);
+        }
     }
 }
