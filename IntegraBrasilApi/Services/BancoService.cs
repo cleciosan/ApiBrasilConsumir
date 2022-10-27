48fbb6c5252efacaa0d0cfa54d7f532b76bd4bc3
cleciosan <cleciosan@hotmail.com>
2022-10-27 15:17:19 -0300

    Implementar servico BancoService

--- IntegraBrasilApi/Services/BancoService.cs
+++ IntegraBrasilApi/Services/BancoService.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.Dtos;
+using IntegraBrasilApi.Interfaces;
 
 namespace IntegraBrasilApi.Services
 {
-    public class BancoService
+    public class BancoService : IBancoService
     {
-        
+        public Task<ResponseGenerico<BancoResponse>> BuscaBancos(string codigoBanco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseGenerico<List<BancoResponse>>> BuscarTodosBancos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
