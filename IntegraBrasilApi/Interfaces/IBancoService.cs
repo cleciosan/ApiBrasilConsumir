527c3f2d4d92c5bd2a6537bff5810eeb73ff4166
cleciosan <cleciosan@hotmail.com>
2022-10-27 15:14:41 -0300

    Implementar metodos na Interface IBancoService

--- IntegraBrasilApi/Interfaces/IBancoService.cs
+++ IntegraBrasilApi/Interfaces/IBancoService.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.Dtos;
 
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IBancoService
     {
-        
+        Task<ResponseGenerico<List<BancoResponse>>> BuscarTodosBancos();
+
+        Task<ResponseGenerico<BancoResponse>> BuscaBancos(string codigoBanco);
     }
 }
