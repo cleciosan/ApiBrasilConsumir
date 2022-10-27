918ab85a9cb2657212ba7b14afc37737dfcaf2af
cleciosan <cleciosan@hotmail.com>
2022-10-27 15:10:01 -0300

    Implementar metodo BuscarTodosBancos na BrasilApiRest

--- IntegraBrasilApi/Rest/BrasilApiRest.cs
+++ IntegraBrasilApi/Rest/BrasilApiRest.cs
@@ -37,9 +37,31 @@ namespace IntegraBrasilApi.Rest
             
         }
 
-        public Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
+        public async Task<ResponseGenerico<List<BancoModel>>> BuscarTodosBancos()
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/banks/v1");
+
+            var response = new ResponseGenerico<List<BancoModel>>();
+
+            using(var client = new HttpClient())
+            {
+                var responseBrasilApi = await client.SendAsync(request);
+                var contentResp = await responseBrasilApi.Content.ReadAsStringAsync();
+                var objResponse = JsonSerializer.Deserialize<List<BancoModel>>(contentResp);
+
+                if (responseBrasilApi.IsSuccessStatusCode)
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.DadosRetorno = objResponse;
+                }
+                else
+                {
+                    response.CodigoHttp = responseBrasilApi.StatusCode;
+                    response.ErroRetorno = JsonSerializer.Deserialize<ExpandoObject>(contentResp);
+                }
+
+                return response;   
+            }
         }
 
         public Task<ResponseGenerico<BancoModel>> BuscarBanco(string codigoBanco)
